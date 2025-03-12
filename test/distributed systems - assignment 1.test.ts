1e0ad20a2c42aba1bf73af81a10c37be55ed06df
zft666zft <2810458566@qq.com>
2025-03-12 16:28:09 +0000

    Setup for assignment1

---
+++ test/distributed systems - assignment 1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DistributedSystemsAssignment1 from '../lib/distributed systems - assignment 1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/distributed systems - assignment 1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DistributedSystemsAssignment1.DistributedSystemsAssignment1Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
